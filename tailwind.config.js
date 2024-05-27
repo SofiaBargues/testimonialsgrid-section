0848d7b1d93fed9c3242aa124d17a799cddfe1c5
Sofia Bargues <barguessofia@gmail.com>
2024-05-27 04:22:51 +0100

    cuotes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
   },
   plugins: [],
 };
